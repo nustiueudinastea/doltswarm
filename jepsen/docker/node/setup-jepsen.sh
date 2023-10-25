de02e9725b82efc8cfb65d7107415cf9a432a857
Alex Giurgiu <alex@giurgiu.io>
2023-10-25 13:34:09 +0300

    will take a different testing strategy for now

--- jepsen/docker/node/setup-jepsen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# We add our hostname to the shared volume, so that control can find us
-echo "Adding hostname to shared volume" >> /var/log/jepsen-setup.log
-echo `hostname` >> /var/jepsen/shared/nodes
--- jepsen/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject jepsen.doltswarm "0.1.0-SNAPSHOT"
-  :description "Jepsen test for doltswarm"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :main jepsen.doltswarm
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.3"]]
-  :repl-options {:init-ns jepsen.doltswarm})
